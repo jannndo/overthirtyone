c23596ce84d8e676494a2fde9aa75393ab809469
jannndo <jan.klatik.code@gmail.com>
2022-09-01 23:44:00 +0200

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>>31</title>
+
+        @vite('resources/css/app.css')
+
+    </head>
+    <body>
+        <div class="container mx-auto">
+            <div class="flex justify-center align-center h-screen">
+                <div class="grid grid-cols-1">
+                    
+                    
+                    <div class="p-5 text-3xl font-bold text-gray-200 text-center">
+                        <span class="align-bottom">Overthirtyone</span>
+                    </div> 
+                    <div class="p-5 text-6xl font-bold text-gray-400 text-center">
+                        <span>>31</span>
+                    </div> 
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
